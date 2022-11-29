8f55d08cfd650d63e3f339843ec562c75c6aebc8
Chloe Havel <113055595+Chloe-Havel@users.noreply.github.com>
2022-11-29 11:08:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ICS2O-PWA-Template-HTML
+
+[![Mr Coxall's Super Linter](https://github.com/Chloe-Havel/ICS2O-PWA-Test-1/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/Chloe-Havel/ICS2O-PWA-Test-1/actions)
+
+[![Run on Repl.it](https://repl.it/badge/github/Chloe-Havel/ICS2O-PWA-Test-1)](https://repl.it/github/Chloe-Havel/ICS2O-PWA-Test-1)
+
+This site can be found at: [https://Chloe-Havel.github.io/ICS2O-PWA-Test-1/](https://Chloe-Havel.github.io/ICS2O-PWA-Test-1/)
---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2020 Chloe Havel All rights reserved
+//
+// Created by: Chloe Havel
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test-1/sw.js", {
+    scope: "/ICS2O-PWA-Test-1/",
+  })
+}
+
+/**
+ * This function displays an alert.
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
