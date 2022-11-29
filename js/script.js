25bc436e8c748ecb4ab3c5e2d1c782722076bb92
Chloe Havel <113055595+Chloe-Havel@users.noreply.github.com>
2022-11-29 11:09:56 -0500

    added what was in 5-02

--- js/script.js
+++ js/script.js
@@ -4,20 +4,35 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-"use strict"
-
 /**
  * Check servie worker.
  */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test-1/sw.js", {
-    scope: "/ICS2O-PWA-Test-1/",
+ if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS20-Unit-5-02/sw.js", {
+    scope: "/ICS20-Unit-5-02/",
   })
+}
+/**
+ * This function updates the slider value.
+ */
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
 }
 
 /**
- * This function displays an alert.
+ * This function displays the slider value.
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  buttonOnChecked = document.getElementById("flash1").checked
+
+  if (buttonOnChecked == true) {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+    document.getElementById("radio-button-value").innerHTML =
+      "<p>Value is: " + randomNumber + " </p>"
+  } else {
+    let randomNumber = Math.floor(Math.random() * 6) + 1
+    randomNumber = randomNumber * -1
+    document.getElementById("radio-button-value").innerHTML =
+      "<p>Value is: " + randomNumber + " </p>"
+  }
 }
